a39fe35d3da22869b0a9a5ea94f85e9392586ea3
Roberto <robertobraun87@gmail.com>
2024-04-29 20:58:44 -0300

    calse 29-04

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,24 @@
 function mostrar(){
-    const nombre=document.getElementById("inp_nombre").value
-    const apellido=document.getElementById("inp_apellido").value
-    let mensaje='Inspeccionando consola, '+apellido+' '+nombre
+    const d1=document.getElementById("inp_d1").value
+    const d2=document.getElementById("inp_d2").value
+    
+    const op=document.getElementById("slt_operacion").value
+    let resultado=null
+    if (op == 0) {
+        resultado = Number(d1) + Number(d2)        
+    }
+    if (op == 1) {
+        resultado = d1 - d2
+    }
+    if (op == 2) {
+        resultado = d1 * d2
+    }
+    if (op == 3) {
+        resultado = d1 / d2
+    }
+    //alert(resultado)
+    let mensaje='El resultado es= '+resultado
+    document.getElementById("h_resultado").textContent = mensaje
     console.log(mensaje)
 }
 
