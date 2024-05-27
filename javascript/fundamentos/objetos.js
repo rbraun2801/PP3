87216e064698994ca0866aadc5e52a14b60f30e7
Roberto <robertobraun87@gmail.com>
2024-05-27 19:59:02 -0300

    clase 27-05

---
+++ javascript/fundamentos/objetos.js
@@ -0,0 +1,15 @@
+const empleados={
+    nombre:'Juan',
+    apellido:'Rodriguez',
+    dni:32323232,
+    correo:{
+        gmail:'juan@gmail.com',
+        outlook:'juan@outlook.com'
+    }
+}
+
+const {gmail,outlook}=empleados.correo
+
+console.log(`
+    ${gmail}
+`)
